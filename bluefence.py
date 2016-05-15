517f965cfb6e51341c9801443189f93d9e4087d6
Dominic Giebert <giebert@in.tum.de>
2016-05-15 23:12:10 +0200

    Wating now in Services

--- bluefence.py
+++ bluefence.py
@@ -1,7 +1,8 @@
-'''Bluefence module including the connection services'''
 #!/usr/bin/python
+'''Bluefence module including the connection services'''
 
 import os
+import sys
 import argparse
 import time
 import bluetooth
@@ -9,12 +10,12 @@ import bluetooth
 # Parse the args from command line
 parser = argparse.ArgumentParser(description='Bluefence utility')
 parser.add_argument('-v', '--verbose', help='Display verbose help', action="store_true")
-parser.add_argument('--delay', metavar='SECONDS', type=int, default=5,
+parser.add_argument('--delay', metavar='SECONDS', type=int, default=10,
                     help='Time between two scans')
-parser.add_argument('--cmd', metavar='COMMAND', type=str,
+parser.add_argument('-c', '--cmd', metavar='COMMAND', type=str,
                     default='dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock',
                     help='Time between two scans')
-parser.add_argument('--missed', metavar='NUMBER', type=int, default=3,
+parser.add_argument('-m', '--missed', metavar='NUMBER', type=int, default=1,
                     help='Number of missed scans to trigger lock')
 parser.add_argument('-w', '--wait', action="store_true",
                     help='Disables the need for your device to be connected initially')
@@ -53,13 +54,19 @@ class Connection(object):
         ''' interface '''
         pass
 
+    def connect(self):
+        '''interface'''
+        pass
+
 class DistanceService(Connection):
     '''Service if distance mode is enabled'''
     def __init__(self, btAddr, debug):
         self.debug = debug
         self.distance = 0
+        self.away_counter = 0
         if self.debug:
             print('\033[92m[STATUS]\033[0m', 'Using distance mode')
+            sys.stdout.flush()
         self.bluetooth_addr = btAddr
 
     # Taken and modified from blueproximity.py
@@ -71,6 +78,7 @@ class DistanceService(Connection):
             self.distance = -255
             if self.debug:
                 print('\033[91m[ERROR]\033[0m Device is not connected')
+                sys.stdout.flush()
         # Extract the rssi and use it as an indicator for the distance
         else:
             self.distance = int(rssi[0].split(':')[1].strip(' '))
@@ -80,6 +88,7 @@ class DistanceService(Connection):
         if self.debug:
             print('\033[92m[STATUS]\033[0m', 'Device is', self.status,
                   'for', self.away_counter*5, 'seconds with a distance of', self.distance)
+            sys.stdout.flush()
 
     def evaluate(self):
         '''Check if the distance is smaller then given and otherwise trigger away'''
@@ -94,13 +103,27 @@ class DistanceService(Connection):
             self.away_counter = 0
             self.status = 'gone'
 
+    def connect(self):
+        rssi = os.popen("hcitool rssi " + self.bluetooth_addr + " 2>/dev/null").readlines()
+        if rssi != []:
+            return bluetooth.lookup_name(args.ADDR, timeout=args.delay)
+
+        if args.wait:
+            print('\033[93m[STATUS]\033[0m Device is not paired')
+            time.sleep(args.delay)
+            return self.connect()
+
+
+
 class NameService(Connection):
     '''Service if distance is disable just querry for the device'''
     def __init__(self, btAddr, debug):
         self.debug = debug
         self.name = ''
+        self.away_counter = 0
         if self.debug:
             print('\033[92m[STATUS]\033[0m', 'Using name mode')
+            sys.stdout.flush()
         self.bluetooth_addr = btAddr
 
     # Taken form bluefence, this does not need your device to be conncted
@@ -113,6 +136,7 @@ class NameService(Connection):
         if self.debug:
             print('\033[92m[STATUS]\033[0m', 'Device is', self.status,
                   'for', self.away_counter*5, 'seconds')
+            sys.stdout.flush()
 
     def evaluate(self):
         '''Check if the device is in range and otherwise trigger away'''
@@ -127,6 +151,17 @@ class NameService(Connection):
             self.away_counter = 0
             self.status = 'gone'
 
+    def connect(self):
+        name = bluetooth.lookup_name(args.ADDR, timeout=args.delay)
+        if name:
+            return name
+
+        if args.wait:
+            print('\033[93m[STATUS]\033[0m Waiting for device')
+            time.sleep(args.delay)
+            return self.connect()
+
+
 def main():
     '''Main Methond'''
     # The command to run when the device is out of range
@@ -135,23 +170,13 @@ def main():
         print('\033[92m[STATUS]\033[0m Checking for device')
 
     try:
-        name = ""
+        connection = create_connection()
 
-        if args.wait:
-            while True:
-                name = bluetooth.lookup_name(args.ADDR, timeout=5)
-                print('\033[92m[STATUS]\033[0m Waiting for devices')
-                if name:
-                    break
-        else:
-            # initial check, see if mentioned BT device active. If it's not, clean exit
-            name = bluetooth.lookup_name(args.ADDR, timeout=5)
-
+        name = connection.connect()
         if name:
             if args.verbose:
                 print('\033[93m[STATUS]\033[0m Found', args.ADDR, 'as', name)
-
-            connection = create_connection()
+                sys.stdout.flush()
 
             while True:
                 connection.measure()
@@ -163,10 +188,12 @@ def main():
 
         else:
             print('\033[91m[ERROR]\033[0m Bluetooth device is not active')
+            sys.stdout.flush()
 
     # this usually happen when your PC's bluetooth is disabled.
     except Exception:
         print('\033[91m[ERROR]\033[0m Bluetooth is not active')
+        sys.stdout.flush()
         if args.verbose:
             print(Exception)
 
